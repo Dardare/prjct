0ea395603c54d59d54b6f0538b0c83ce879a7c73
Dardare <infinitymyname@yandex.ru>
2016-03-01 15:35:52 +0300

    Added files via upload

--- R/creating_samples.R
+++ R/creating_samples.R
@@ -1,35 +1,38 @@
-white <-  which(diff(data$StimulusType)>0)+1
-black <- which(diff(data$Flashing)>0)+1
-black <- black[!(black %in% white)]
-
-black <- black[seq(by=4, length.out=length(white))]
-
-bigNiggas <- lapply(white, function(s){
-  kek[(s-120):(s+119), ]
-})
-smallNiggas <- lapply(black, function(s){
-  kek[(s-120):(s+119), ]
-})
-
-
-
-epochA <- matrix(ncol = 1440, nrow = 0)
-
-for (i in bigNiggas) {
-  epochA <- rbind(epochA, as.numeric(abs(mvfft(i))))
+function (StimulusType, Flashing, laplas) {
+  p300stim <- which(diff(StimulusType)>0)+1
+  otherstim <- which(diff(Flashing)>0)+1
+  otherstim <- otherstim[!(otherstim %in% p300stim)]
+  
+  otherstim <- otherstim[seq(by=4, length.out=length(p300stim))]
+  
+  p300list <- lapply(p300stim, function(s){
+    laplas[(s-120):(s+119), ]
+  })
+  otherstimlist <- lapply(otherstim, function(s){
+    laplas[(s-120):(s+119), ]
+  })
+  
+  
+  
+  epochA <- matrix(ncol = 1440, nrow = 0)
+  
+  for (i in p300list) {
+    epochA <- rbind(epochA, as.numeric(abs(mvfft(i))))
+  }
+  
+  
+  epochB <- matrix(ncol = 1440, nrow = 0)
+  
+  for (i in otherstimlist) {
+    epochB <- rbind(epochB, as.numeric(abs(mvfft(i))))
+  }
+  
+  randomf <- rbind(epochB, epochA)
+  
+  logicvector <- vector(mode = "logical", length = 180)
+  logicvector[1:90] <- TRUE
+  
+  random <- data.frame(random, logicvector)
+  return(random)
 }
-
-
-epochB <- matrix(ncol = 1440, nrow = 0)
-
-for (i in smallNiggas) {
-  epochB <- rbind(epochB, as.numeric(abs(mvfft(i))))
-}
-
-random <- rbind(epochB, epochA)
-
-honesty <- vector(mode = "logical", length = 180)
-honesty[1:90] <- TRUE
-
-random <- data.frame(random, honesty)
 
--- R/forest.R
+++ R/forest.R
@@ -1,8 +1,7 @@
-library(h2o)
-localH2O = h2o.init()
-
-
-random.hex <- as.h2o(random, destination_frame = "random")
-random.rf = h2o.randomForest(x = c(1:1440), y = 1441, training_frame = random.hex, ntrees = 800, max_depth = 100)
-summary(random)
-print(random.rf)
+function (random) {
+  localH2O = h2o.init()
+  random.hex <- as.h2o(random, destination_frame = "random")
+  random.rf = h2o.randomForest(x = c(1:1440), y = 1441, training_frame = random.hex, ntrees = 800, max_depth = 100)
+  print(random.rf)
+  return(random.rf)
+  }
