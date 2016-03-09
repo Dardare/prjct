7015763f7c421e84e80339af34a5fb5b0c94d436
Nastya <mayenok@gmail.com>
2016-03-09 16:02:54 +0300

    new

--- R/testFFT.R
+++ R/testFFT.R
@@ -1,9 +1,8 @@
-testFFT <- function(file, wndSize=10){
-  file <- "D:/darisiy/data/data/AAS011R01.mat"
-  loadData(file) #load
-  laplas <- Laplas(signals) #creating laplacian electrodes
-  samples <- creating_samples(StimulusType, Flashing, laplas) #generate features
-  frame <- Frame(samples) #fft and combine
+testFFT <- function(file, wndSize){
+  data <- loadData(file) #load
+  laplas <- Laplas(data$signal) #creating laplacian electrodes
+  samples <- creating_samples(data$StimulusType, data$Flashing, laplas) #generate features
+  frame <- Frame(samples, wndSize) #fft and combine
   test <- Random(frame) #RF
   print(test)
 }
