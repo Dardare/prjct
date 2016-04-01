31172c84c57f00cae78a8af6d65791b9e1231904
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2016-04-01 16:34:39 +0300

    Fixed heatmaps

--- R/heatmatrix.R
+++ R/heatmatrix.R
@@ -1,11 +1,11 @@
-heatmap_laplacian <- function(test) {
+heatmap_laplacian <- function(test, channels=6) {
   importance <- test@model$variable_importances
   importance[,1] <- gsub("V", "", importance[,1])
   importance[,1] <- strtoi(importance[,1])
   importance <- importance[order(importance[,1]), ]
-  heatmatrix <- matrix(importance$percentage, nrow=6, byrow = T)
+  heatmatrix <- matrix(importance$percentage, nrow=channels, byrow = T)
 
-  heatmap(heatmatrix, Rowv=NA, Colv=NA, col = heat.colors(256), scale="column")
+  ggplot(melt(heatmatrix), aes(Var2, Var1, fill=value))+ scale_fill_gradient(low = "white", high = "steelblue") + geom_tile()
 }
 
 heatmap_fft <- function(test, windowSize) {
@@ -17,13 +17,14 @@ heatmap_fft <- function(test, windowSize) {
   heatarray <- array(importance$percentage, dim=c(windowSize,6,nrow(importance)/6/windowSize))
 
   print(
-    heatmap(apply(heatarray, c(2,1), mean), Rowv=NA, Colv=NA, col = heat.colors(256), scale="column", main = 'Hz x Ch')
+    ggplot(melt(apply(heatarray, c(2,1), sum)), aes(Var2, Var1, fill=value))+ scale_fill_gradient(low = "white", high = "steelblue") + geom_tile() + ggtitle('Hz x Ch')
+
   )
   print(
-    heatmap(apply(heatarray, c(2,3), mean), Rowv=NA, Colv=NA, col = heat.colors(256), scale="column", main = 'T x Ch')
+    ggplot(melt(apply(heatarray, c(2,3), sum)), aes(Var2, Var1, fill=value))+ scale_fill_gradient(low = "white", high = "steelblue") + geom_tile() + ggtitle('T x Ch')
   )
   print(
-    heatmap(apply(heatarray, c(1,3), mean), Rowv=NA, Colv=NA, col = heat.colors(256), scale="column", main = 'T x Hz')
+    ggplot(melt(apply(heatarray, c(1,3), sum)), aes(Var2, Var1, fill=value))+ scale_fill_gradient(low = "white", high = "steelblue") + geom_tile() + ggtitle('T x Hz')
   )
   #  plot(
   #    ggplot(melt(heatmatrix), aes(x=Var2, y=Var1, fill=value)) + scale_fill_continuous()
