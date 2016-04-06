a588d71240debe0c0e10dedd3155bb6d0ec01643
Nastya <mayenok@gmail.com>
2016-04-06 16:46:46 +0300

    electrodes

--- R/testFFT.R
+++ R/testFFT.R
@@ -1,6 +1,6 @@
 testFFT <- function(file, buildFeature){
   data <- loadData(file) #load
-  laplas <- Laplas(data$signal) #creating laplacian electrodes
+  laplas <- Value(data$signal) #creating laplacian electrodes
   samples <- creating_samples(data$StimulusType, data$Flashing, laplas) #generate features
   frame <- Frame(samples, wndSize, buildFeature) #fft and combine
   test <- Random(frame) #RF
