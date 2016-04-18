dd92b2d18a46667b383429d69c95416fdc36ab62
dardare <infinitymyname@yandex.ru>
2016-04-18 15:44:42 +0300

    Combinations

---
+++ R/Comb_mean.R
@@ -0,0 +1,13 @@
+Comb_mean <- function(wndSize, wndStep){
+
+    partition <- seq(from = 1, to = 240-wndSize+1, by = wndStep)
+
+    function(i){
+      wnds <- lapply(partition, function(s) {
+        i[1:wndSize + s-1, ]
+      })
+      tmp <- do.call(c, lapply(wnds, function(k){colMeans(k)}))
+      tmp <- combn(x = tmp, m = 2, FUN = function(x) x[1] - x[2])
+      as.vector(tmp)
+    }
+  }
