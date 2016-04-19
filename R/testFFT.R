280905ba977a427dbbcd23811a4d63d666e8e571
Nastya <mayenok@gmail.com>
2016-04-19 13:09:57 +0300

    bla

--- R/testFFT.R
+++ R/testFFT.R
@@ -1,12 +1,11 @@
 testFFT <- function(n, buildFeature){
+  samples <- c()
   for (i in n) {
     nam <- paste("test", i, sep = ".")
     data <- loadData(i) #load
     laplas <- Value(data$signal) #creating laplacian electrodes
-    samples <- creating_samples(data$StimulusType, data$Flashing, laplas) #generate features
-    frame <- Frame(samples, wndSize, buildFeature) #fft and combine
-    test <- Random(frame) #RF
-    print(test)
-    assign(nam, test, envir = .GlobalEnv)
+    samples <- rbind(samples, creating_samples(data$StimulusType, data$Flashing, laplas)) #generate features
   }
+  frame <- Frame(samples, wndSize, buildFeature) #fft and combine
+  test <- Random(frame) #RF
 }
