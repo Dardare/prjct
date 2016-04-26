a0740d667875e55018f9acd9ee6500b941b8e5ab
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2016-04-26 14:10:46 +0300

    Ura

--- R/Comb_mean.R
+++ R/Comb_mean.R
@@ -1,13 +1,25 @@
-Comb_mean <- function(wndSize, wndStep){
+Comb_mean <- function(wndSize){
 
-    partition <- seq(from = 1, to = 240-wndSize+1, by = wndStep)
+  partition <- seq(from = 1, to = 240-wndSize+1, by = wndSize)
 
-    function(i){
-      wnds <- lapply(partition, function(s) {
-        i[1:wndSize + s-1, ]
-      })
-      tmp <- do.call(c, lapply(wnds, function(k){colMeans(k)}))
-      tmp <- combn(x = tmp, m = 2, FUN = function(x) x[1] - x[2])
-      as.vector(tmp)
-    }
+  function(i){
+    wnds <- lapply(partition, function(s) {
+      i[1:wndSize + s-1, ]
+    })
+    tmp <- do.call(c, lapply(wnds, function(k){colMeans(k)}))
+    tmp <- combn(x = tmp, m = 2, FUN = function(x) x[1] - x[2])
+    as.vector(tmp)
   }
+}
+
+chan_mean <- function(wndSize){
+
+  partition <- seq(from = 1, to = 240-wndSize+1, by = wndSize)
+
+  function(i){
+    wnds <- lapply(partition, function(s) {
+      i[1:wndSize + s-1, ]
+    })
+    as.vector(wnds)
+  }
+}
