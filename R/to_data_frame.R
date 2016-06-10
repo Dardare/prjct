200b802ab7411e846ea188c029f63803fed22a26
Nastya <mayenok@gmail.com>
2016-06-10 14:05:11 +0300

    vay

--- R/to_data_frame.R
+++ R/to_data_frame.R
@@ -1,5 +1,5 @@
-cmb_frame <- function(target, nontarget){
-  features <- as.data.frame(as.data.frame(t(sapply(c(target, nontarget), function(k){as.vector(k)}))))
+cmb_frame <- function(target, nontarget, buildFeature){
+  features <- as.data.frame(as.data.frame(t(sapply(c(target, nontarget), buildFeature))))
   features$class <- F
   features$class[1:length(target)] <- T
   return(features)
